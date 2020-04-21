0a57780db6e81d1139bb306536f3477b4d26f500
BuildTools <unconfigured@null.spigotmc.org>
2020-04-21 02:18:49 -0400

    Initial commit

---
+++ src/main/java/me/benjixinator/easyenchanting/EasyEnchanting.java
@@ -0,0 +1,28 @@
+package me.benjixinator.easyenchanting;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyEnchanting extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+
+
+
+        return false;
+    }
+}
